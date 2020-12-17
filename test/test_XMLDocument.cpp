bac8e58880ab4198590cad84b8fc6feb436a17d1
Aaron G. Tumulak <agtumulak@gmail.com>
2020-12-16 21:37:18 -0500

    Use uniform initialization in XMLDocument unit test

--- test/test_XMLDocument.cpp
+++ test/test_XMLDocument.cpp
@@ -2,12 +2,12 @@
 #include "catch2/catch.hpp"
 
 TEST_CASE("valid XML file passes") {
-  REQUIRE_NOTHROW(XMLDocument("test_XMLDocument_valid.xml"));
+  REQUIRE_NOTHROW(XMLDocument{"test_XMLDocument_valid.xml"});
 }
 
 TEST_CASE("invalid XML file fails") {
   REQUIRE_THROWS_WITH(
-      XMLDocument("test_XMLDocument_invalid.xml"),
+      XMLDocument{"test_XMLDocument_invalid.xml"},
       Catch::Matchers::Contains(
           "line 4: column 21\n"
           "no declaration found for element 'unexpected_node'"));
