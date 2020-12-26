7e38422ee612177990cc20a62a8dd3f21801f7c7
Aaron G. Tumulak <agtumulak@gmail.com>
2020-12-26 13:17:11 -0500

    Remove Xerces-C++ from minimc main()

--- src/minimc.cpp
+++ src/minimc.cpp
@@ -1,10 +1,6 @@
-#include "xercesc/util/PlatformUtils.hpp"
-
 #include <iostream>
 
 int main(int argc, char* argv[]) {
   std::cout << "Welcome to MiniMC!" << std::endl;
-  xercesc::XMLPlatformUtils::Initialize();
-  xercesc::XMLPlatformUtils::Terminate();
   return 0;
 }
