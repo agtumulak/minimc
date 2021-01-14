d6decbac06e979b7e71b514c7926631504c9ec3a
Aaron G. Tumulak <agtumulak@gmail.com>
2021-01-13 22:14:50 -0500

    Add executable runminimc

--- src/minimc.cpp
+++ src/minimc.cpp
@@ -1,6 +1,16 @@
+#include "FixedSource.hpp"
+#include "XMLDocument.hpp"
+
 #include <iostream>
+#include <stdexcept>
 
 int main(int argc, char* argv[]) {
   std::cout << "Welcome to MiniMC!" << std::endl;
+  if (argc != 2){
+    throw std::runtime_error("MiniMC accepts exactly one argument");
+  }
+  XMLDocument doc{argv[1]};
+  FixedSource problem{doc.root};
+  problem.Solve();
   return 0;
 }
