04454eb647dd536ea1ae3b380afef521c5cf55d2
Aaron G. Tumulak <agtumulak@gmail.com>
2021-09-08 23:34:46 -0400

    Move thermal scattering test to separate input file
    Currently only tests if thermal scattering occurs without throwing.
    Actual testing of outgoing distributions is not done.

--- test/test_ThermalScattering.cpp
+++ test/test_ThermalScattering.cpp
@@ -1,13 +1,13 @@
-#include "ThermalScattering.hpp"
+#include "FixedSource.hpp"
+#include "World.hpp"
 #include "XMLDocument.hpp"
 #include "catch2/catch.hpp"
 
-TEST_CASE("create TSL data") {
-  XMLDocument doc{"simple_continuous.xml"};
-  const auto tsl_node =
-      doc.root
-          .select_node("nuclides/continuous/nuclide[@name='hydrogen']/neutron/"
-                       "scatter/tsl")
-          .node();
-  REQUIRE_NOTHROW(ThermalScattering{tsl_node});
+TEST_CASE("thermal scattering") {
+  XMLDocument doc{"test_ThermalScattering.xml"};
+  const World w{doc.root};
+  FixedSource f{doc.root};
+  // TODO: Check result of estimators, similar to `runsab` from
+  //       `feature/sab-validation`
+  REQUIRE_NOTHROW(f.Solve());
 }
