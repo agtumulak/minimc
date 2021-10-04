31c8c46490c4e77ed2581b948fc55f83b75d6215
Aaron G. Tumulak <agtumulak@gmail.com>
2021-10-04 13:45:59 -0400

    Fix about-axis Direction factory function header

--- src/Point.hpp
+++ src/Point.hpp
@@ -56,11 +56,6 @@ class Direction : public Point {
 public:
   /// @brief Constructs an isotropically sampled Direction
   static Direction CreateIsotropic(RNG& rng) noexcept;
-  /// @brief Constructs a Direction from a `PointType` node
-  /// @details `PointType` is a `complexType` defined in the minimc XML schema.
-  Direction(const pugi::xml_node& pointtype_node) noexcept;
-  /// @brief Constructs a Direction with the given components
-  Direction(const Real& x, const Real& y, const Real& z) noexcept;
   /// @brief Constructs a new Direction that has a given cosine @f$ \mu @f$
   ///        with respect to an exsiting Direction.
   /// @details Given a Direction @f$ \boldsymbol{\Omega}^{\prime} @f$, the new
@@ -72,7 +67,13 @@ public:
   /// @param phi The azimuthal angle about @f$ \boldsymbol{\Omega}^{\prime}
   ///            @f$. This is largely arbitrary since scattering is usually
   ///            azimuthally symmetric.
-  Direction(const Direction& d, const Real& mu, const Real& phi) noexcept;
+  static Direction CreateAboutDirection(
+      const Direction& d, const Real& mu, const Real& phi) noexcept;
+  /// @brief Constructs a Direction from a `PointType` node
+  /// @details `PointType` is a `complexType` defined in the minimc XML schema.
+  Direction(const pugi::xml_node& pointtype_node) noexcept;
+  /// @brief Constructs a Direction with the given components
+  Direction(const Real& x, const Real& y, const Real& z) noexcept;
   /// @brief Move constructor. Constructs the Direction with the contents of a
   ///        Point. The given Point is normalized.
   Direction(Point&& other) noexcept;
