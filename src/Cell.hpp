1505d914262f2846dacdde205df39b1b57e46e8d
Aaron G. Tumulak <agtumulak@gmail.com>
2021-10-30 12:13:14 -0400

    Remove seemingly completed todo

--- src/Cell.hpp
+++ src/Cell.hpp
@@ -16,7 +16,6 @@ class ScalarField;
 
 /// @brief A subset of @f$ \mathbb{R}^{3} @f$ defined by constructive solid
 ///        geometry (CSG) surfaces
-/// @todo Add support for other temperature ScalarField types
 class Cell {
 private:
   // A polymorphic vector of CSGSurface objects. Multiple Cell objects can
