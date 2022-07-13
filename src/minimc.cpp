f9dabdd6cd5edba63c5d6e3082f79a7c9c724e4f
Aaron G. Tumulak <agtumulak@gmail.com>
2022-07-13 09:23:29 -0400

    WIP pybind11

--- src/minimc.cpp
+++ src/minimc.cpp
@@ -5,13 +5,24 @@
 #include <memory>
 #include <stdexcept>
 
-int main(int argc, char* argv[]) {
-  std::cout << "Welcome to MiniMC!" << std::endl;
-  if (argc != 2) {
-    throw std::runtime_error("MiniMC accepts exactly one argument");
-  }
-  auto driver = Driver::Create(argv[1]);
-  const auto& result = driver->Solve();
-  std::cout << result.to_string();
+
+#include "pybind11/pybind11.h"
+#include "pybind11/embed.h"
+
+namespace py = pybind11;
+
+PYBIND11_EMBEDDED_MODULE(fast_calc, m){
+  m.def("add", [](int i, int j){
+    return i + j;
+  });
+}
+
+int main() {
+  py::scoped_interpreter guard{};
+  auto fast_calc = py::module_::import("fast_calc");
+  auto result = fast_calc.attr("add")(1,2).cast<int>();
+  py::print(result);
+  // py::exec("import sys; sys.path.append('/Users/atumulak/.local/share/virtualenvs/pyminimc-tdgjIrhJ/lib/python3.10/site-packages/'); import numpy as np; print(np.array([1,2,3]))");
+   py::exec("import sys; sys.path.append('/Users/atumulak/.local/share/virtualenvs/pyminimc-tdgjIrhJ/lib/python3.10/site-packages/'); import numpy as np; print(np.array([1,2,3]))");
   return 0;
 }
