6529dcff074652be923759e2a9609c9b891cb698
Aaron G. Tumulak <agtumulak@gmail.com>
2022-12-13 01:21:15 -0500

    Add differential operator sampling versus central difference unit test

--- test/test_Perturbation.cpp
+++ test/test_Perturbation.cpp
@@ -9,7 +9,6 @@
 #include "catch2/matchers/catch_matchers_floating_point.hpp"
 
 #include <cmath>
-#include <cstddef>
 
 TEST_CASE(
     "purely absorbing sphere leakage rate sensitivity to total cross section") {
@@ -24,4 +23,27 @@ TEST_CASE(
       Catch::Matchers::WithinRel(
           expected_sensitivity,
           epsilon::Bernoulli(-expected_sensitivity, f.total_weight)));
+}
+
+TEST_CASE(
+    "nonmultiplying sphere leakage rate sensitivity to total cross section") {
+  // compute sensitivity using differential operator sampling
+  XMLDocument doc{"nonmultiplying_sphere_leakage_totalxs_sensitivity.xml"};
+  const World w{doc.root};
+  FixedSource f{doc.root};
+  const auto dos =
+      f.Solve().front()->sensitivities.front()->GetScore(0, f.total_weight);
+  // perturb capture cross section by +0.05b
+  XMLDocument doc_hi{"nonmultiplying_sphere_leakage_totalxs_perturb_hi.xml"};
+  const World w_hi{doc_hi.root};
+  FixedSource f_hi{doc_hi.root};
+  const auto perturb_hi = f_hi.Solve().front()->GetScore(0, f_hi.total_weight);
+  // perturb capture cross section by -0.05b
+  XMLDocument doc_lo{"nonmultiplying_sphere_leakage_totalxs_perturb_lo.xml"};
+  const World w_lo{doc_lo.root};
+  FixedSource f_lo{doc_lo.root};
+  const auto perturb_lo = f_lo.Solve().front()->GetScore(0, f_lo.total_weight);
+  // compare differential operator sampling against central difference
+  const auto central_difference = (perturb_hi - perturb_lo) / 0.1;
+  REQUIRE_THAT(dos, Catch::Matchers::WithinRel(central_difference, 0.1));
 }
