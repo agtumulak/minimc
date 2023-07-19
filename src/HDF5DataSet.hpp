bb76c3db684af8d7fa378f83ce5a65cecff90b3e
Aaron G. Tumulak <agtumulak@gmail.com>
2023-07-19 04:52:19 -0400

    Add method to obtain offsets for direct access to flattened vectors

--- src/HDF5DataSet.hpp
+++ src/HDF5DataSet.hpp
@@ -3,7 +3,6 @@
 #include "BasicTypes.hpp"
 #include "ContinuousMap.hpp"
 #include "H5Cpp.h"
-#include "autodiff/reverse/var.hpp"
 
 #include <algorithm>
 #include <array>
@@ -14,7 +13,6 @@
 #include <numeric>
 #include <stdexcept>
 #include <string>
-#include <type_traits>
 #include <vector>
 
 /// @brief Loads an HDF5 file created by pandas
@@ -36,18 +34,14 @@
 ///            <a href="https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_hdf.html">
 ///            `pandas.DataFrame.to_hdf`</a> is called.
 /// @tparam D number of dimensions (independent axes) in HDF5 file
-/// @tparam autodiff If true, elements are stored as autodiff::var so that
-///                  they can be used as independent variables in a sensitivity
-///                  calculation
-template <size_t D, bool autodiff = false> class HDF5DataSet {
+template <size_t D> class HDF5DataSet {
 public:
-  using T = std::conditional_t<autodiff, autodiff::var, Real>;
   /// @brief Constructs an HDF5DataSet from an hdf5 file
   /// @details https://support.hdfgroup.org/HDF5/doc/cpplus_RM/readdata_8cpp-example.html
   HDF5DataSet(const std::filesystem::path& hdf5_filepath)
       : axes{ReadPandasAxis(hdf5_filepath)},
-        values{ReadPandasValues(hdf5_filepath)}, strides{ComputeAxisStrides(
-                                                     axes)} {}
+        values{ReadPandasValues(hdf5_filepath)},
+        strides{ComputeAxisStrides(axes)} {}
   /// @brief Returns an instance of ContinuousMap with D levels of nesting
   /// @tparam Args Type of outer indices
   /// @param outer_indices Indices of outer levels; populated during recursion
@@ -60,7 +54,7 @@ public:
       result = at(outer_indices...);
     }
     else {
-      const auto& keys = axes.at(sizeof...(outer_indices));
+      const auto& keys = GetAxis(sizeof...(outer_indices));
       for (size_t index = 0; index < keys.size(); index++) {
         result[keys[index]] = ToContinuousMap(outer_indices..., index);
       }
@@ -76,19 +70,33 @@ public:
   ///          index 4 of level 0, index 2 of level 1, and index 0 of level 2
   ///          with `at(4, 2, 0)`.
   template <typename... Args>
-  T at(size_t index, Args... inner_indices) const {
+  Real at(size_t index, Args... inner_indices) const {
     // compile time error if wrong number of indices
     static_assert(1 + sizeof...(inner_indices) == D);
-    return at_from_base(0, 0, index, inner_indices...);
+    return values.at(GetOffset(index, inner_indices...));
+  }
+  /// @brief Returns the offset or flattened index at the given axis indices
+  /// @details If fewer indices than axes are given, an offset is returned. If
+  ///          the number of indices matches the number of axes, the
+  ///          corresponding flattened index to an element is returned.
+  template <typename... Args>
+  size_t GetOffset(size_t index, Args... inner_indices) const {
+    // compile time error if too many indices
+    static_assert(1 + sizeof...(inner_indices) <= D);
+    return GetOffsetFromBase(0, 0, index, inner_indices...);
+  }
+  /// @brief Get values of a given axis
+  const std::vector<double>& GetAxis(size_t level) const noexcept {
+    return axes.at(level);
   }
   /// @brief Returns total number of elements in dataset
   size_t size() const noexcept { return values.size(); }
   /// @brief Outer vector contains each axis. Inner vector contains values of
-  ///        an axis (C++ Core Guidelines C.131)
+  ///        an axis. (C++ Core Guidelines C.131)
   const std::array<std::vector<double>, D> axes;
-  /// @brief The values of the rectangular grid are stored in a flattened array.
+  /// @brief The values of the rectangular grid are stored in a flattened array
   ///        (C++ Core Guidelines C.131)
-  const std::vector<T> values;
+  const std::vector<double> values;
 
 private:
   // Helper function to read HDF5 file from pandas and return flattened array.
@@ -125,23 +133,18 @@ private:
     return result;
   }
   // Helper function to read flattened array from HDF5 file
-  static std::vector<T>
+  static std::vector<double>
   ReadPandasValues(const std::filesystem::path& hdf5_filepath) {
-    std::vector<double> buffer;
+    std::vector<double> result;
     const H5::H5File file(hdf5_filepath.string(), H5F_ACC_RDONLY);
     // Read elements into result
     const auto block_dataset =
         file.openGroup("/pandas").openDataSet("block0_values");
-    buffer = std::vector<double>(
-        static_cast<size_t>(block_dataset.getSpace().getSimpleExtentNpoints()));
-    block_dataset.read(buffer.data(), H5::PredType::NATIVE_DOUBLE);
-    if constexpr (autodiff) {
-      std::vector<T> result(buffer.cbegin(), buffer.cend());
-      return result;
-    }
-    else {
-      return buffer;
-    }
+    result = std::vector<double>(
+        static_cast<size_t>(block_dataset.getSpace().getSimpleExtentNpoints()),
+        0);
+    block_dataset.read(result.data(), H5::PredType::NATIVE_DOUBLE);
+    return result;
   }
   // Helper function to precompute strides.
   static std::array<size_t, D>
@@ -164,7 +167,7 @@ private:
   // Starting from a given base offset, using innermost levels starting at
   // given level, return value from full flattened array
   template <typename... Args>
-  T at_from_base(
+  size_t GetOffsetFromBase(
       size_t base, size_t level, size_t index, Args... inner_indices) const {
     // runtime error if index is out of range
     if (index >= axes.at(level).size()) {
@@ -173,10 +176,10 @@ private:
           std::to_string(index) + ")");
     }
     if constexpr (sizeof...(inner_indices) == 0) {
-      return values.at(base + index);
+      return base + index;
     }
     else {
-      return at_from_base(
+      return GetOffsetFromBase(
           base + index * strides.at(level), level + 1, inner_indices...);
     }
   }
