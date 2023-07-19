156263e1cde0f07bb40924a63bfe064e559f0cec
Aaron G. Tumulak <agtumulak@gmail.com>
2023-07-19 04:59:16 -0400

    Use IIFE for HDF5DataSet member initialization

--- src/HDF5DataSet.hpp
+++ src/HDF5DataSet.hpp
@@ -39,9 +39,69 @@ public:
   /// @brief Constructs an HDF5DataSet from an hdf5 file
   /// @details https://support.hdfgroup.org/HDF5/doc/cpplus_RM/readdata_8cpp-example.html
   HDF5DataSet(const std::filesystem::path& hdf5_filepath)
-      : axes{ReadPandasAxis(hdf5_filepath)},
-        values{ReadPandasValues(hdf5_filepath)},
-        strides{ComputeAxisStrides(axes)} {}
+      : // IIFE
+        axes{[&hdf5_filepath]() {
+          // Of all the other helper functions this is the one that checks for
+          // file existence just because it happens to be called first.
+          if (!std::filesystem::exists(hdf5_filepath)) {
+            throw std::runtime_error(
+                std::string{"File not found: "} + hdf5_filepath.string());
+          }
+          std::array<std::vector<double>, D> result;
+          const H5::H5File file(hdf5_filepath.string(), H5F_ACC_RDONLY);
+          // Read number of levels in pandas MultiIndex
+          const auto pandas_group = file.openGroup("/pandas");
+          size_t levels;
+          pandas_group.openAttribute("axis1_nlevels")
+              .read(H5::PredType::NATIVE_ULLONG, &levels);
+          if (levels != D) {
+            throw std::runtime_error(
+                hdf5_filepath.string() + ": Expected " + std::to_string(D) +
+                " dimensions, but got " + std::to_string(levels));
+          }
+          // Read each axis into result
+          for (size_t i = 0; i < D; i++) {
+            const auto level_dataset = pandas_group.openDataSet(
+                std::string{"axis1_level"} + std::to_string(i));
+            result[i] = std::vector<double>(
+                static_cast<size_t>(
+                    level_dataset.getSpace().getSimpleExtentNpoints()),
+                0);
+            level_dataset.read(result[i].data(), H5::PredType::NATIVE_DOUBLE);
+          }
+          return result;
+        }()},
+        // IIFE
+        values{[&hdf5_filepath]() {
+          std::vector<double> result;
+          const H5::H5File file(hdf5_filepath.string(), H5F_ACC_RDONLY);
+          // Read elements into result
+          const auto block_dataset =
+              file.openGroup("/pandas").openDataSet("block0_values");
+          result = std::vector<double>(
+              static_cast<size_t>(
+                  block_dataset.getSpace().getSimpleExtentNpoints()),
+              0);
+          block_dataset.read(result.data(), H5::PredType::NATIVE_DOUBLE);
+          return result;
+        }()},
+        // IIFE
+        strides{[this]() {
+          std::array<size_t, D> result;
+          // Element `i` in `result` contains the size of axis `i+1`
+          std::transform(
+              std::next(axes.cbegin(), 1), axes.cend(), result.begin(),
+              [](const auto& axis) { return axis.size(); });
+          // Last element is 1 since the stride at the innermost axis (level `D - 1`)
+          // will be one.
+          result[D - 1] = 1;
+          // Compute strides by partially multiplying each level size beginning at
+          // level `D - 1`
+          std::partial_sum(
+              result.crbegin(), result.crend(), result.rbegin(),
+              std::multiplies());
+          return result;
+        }()} {}
   /// @brief Returns an instance of ContinuousMap with D levels of nesting
   /// @tparam Args Type of outer indices
   /// @param outer_indices Indices of outer levels; populated during recursion
@@ -99,71 +159,6 @@ public:
   const std::vector<double> values;
 
 private:
-  // Helper function to read HDF5 file from pandas and return flattened array.
-  // Of all the other helper function this is the one that checks for file
-  // existence just because it happens to be called first.
-  static std::array<std::vector<double>, D>
-  ReadPandasAxis(const std::filesystem::path& hdf5_filepath) {
-    if (!std::filesystem::exists(hdf5_filepath)) {
-      throw std::runtime_error(
-          std::string{"File not found: "} + hdf5_filepath.string());
-    }
-    std::array<std::vector<double>, D> result;
-    const H5::H5File file(hdf5_filepath.string(), H5F_ACC_RDONLY);
-    // Read number of levels in pandas MultiIndex
-    const auto pandas_group = file.openGroup("/pandas");
-    size_t levels;
-    pandas_group.openAttribute("axis1_nlevels")
-        .read(H5::PredType::NATIVE_ULLONG, &levels);
-    if (levels != D) {
-      throw std::runtime_error(
-          hdf5_filepath.string() + ": Expected " + std::to_string(D) +
-          " dimensions, but got " + std::to_string(levels));
-    }
-    // Read each axis into result
-    for (size_t i = 0; i < D; i++) {
-      const auto level_dataset = pandas_group.openDataSet(
-          std::string{"axis1_level"} + std::to_string(i));
-      result[i] = std::vector<double>(
-          static_cast<size_t>(
-              level_dataset.getSpace().getSimpleExtentNpoints()),
-          0);
-      level_dataset.read(result[i].data(), H5::PredType::NATIVE_DOUBLE);
-    }
-    return result;
-  }
-  // Helper function to read flattened array from HDF5 file
-  static std::vector<double>
-  ReadPandasValues(const std::filesystem::path& hdf5_filepath) {
-    std::vector<double> result;
-    const H5::H5File file(hdf5_filepath.string(), H5F_ACC_RDONLY);
-    // Read elements into result
-    const auto block_dataset =
-        file.openGroup("/pandas").openDataSet("block0_values");
-    result = std::vector<double>(
-        static_cast<size_t>(block_dataset.getSpace().getSimpleExtentNpoints()),
-        0);
-    block_dataset.read(result.data(), H5::PredType::NATIVE_DOUBLE);
-    return result;
-  }
-  // Helper function to precompute strides.
-  static std::array<size_t, D>
-  ComputeAxisStrides(const std::array<std::vector<double>, D>& axes) noexcept {
-    // Initialize result
-    std::array<size_t, D> result;
-    // Element `i` in `result` contains the size of axis `i+1`
-    std::transform(
-        std::next(axes.cbegin(), 1), axes.cend(), result.begin(),
-        [](const auto& axis) { return axis.size(); });
-    // Last element is 1 since the stride at the innermost axis (level `D - 1`)
-    // will be one.
-    result[D - 1] = 1;
-    // Compute strides by partially multiplying each level size beginning at
-    // level `D - 1`
-    std::partial_sum(
-        result.crbegin(), result.crend(), result.rbegin(), std::multiplies());
-    return result;
-  }
   // Starting from a given base offset, using innermost levels starting at
   // given level, return value from full flattened array
   template <typename... Args>
